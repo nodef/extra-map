51c5467d860fbbe62d2775eac263893eb9fb891a
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 02:25:32 +0530

    :watermelon: cool fruit v2.0.42

--- src/entries.ts
+++ src/entries.ts
@@ -1,9 +1,10 @@
+import type {Entries} from './_types';
+
 /**
  * Lists all entries.
  * @param x a map
  */
-function* entries<K, V>(x: Iterable<[K, V]>): IterableIterator<[K, V]> {
-  for(var e of x)
-    yield e;
+function* entries<T, U>(x: Entries<T, U>): IterableIterator<[T, U]> {
+  yield* x;
 }
 export default entries;
