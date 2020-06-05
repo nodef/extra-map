0dc633134de34b360b38664f78df9bfc0637a060
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 03:02:27 +0530

    :watermelon: cool fruit v2.0.59

--- src/some.ts
+++ src/some.ts
@@ -1,14 +1,25 @@
-import type {testFn} from './_types';
+import type {Entries, testFn} from './_types';
+
+function someIf<T, U>(x: Entries<T, U>): boolean {
+  for(var [, v] of x)
+    if(v) return true;
+  return false;
+}
+
+function someTest<T, U>(x: Entries<T, U>, fn: testFn<T, U>, ths: object=null): boolean {
+  for(var [k, v] of x)
+    if(fn.call(ths, v, k, x)) return true;
+  return false;
+}
 
 /**
  * Checks if any value satisfies a test.
  * @param x a map
- * @param fn test function (v, k, x)
+ * @param fn test function (v, k ,x)
  * @param ths this argument
  */
-function some<K, V>(x: Iterable<[K, V]>, fn: testFn<K, V>, ths: object=null) {
-  for(var [k, v] of x)
-    if(fn.call(ths, v, k, x)) return true;
-  return false;
+function some<T, U>(x: Entries<T, U>, fn: testFn<T, U>=null, ths: object=null): boolean {
+  if(fn) return someTest(x, fn, ths);
+  else return someIf(x);
 }
 export default some;
