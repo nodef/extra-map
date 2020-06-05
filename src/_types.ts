9d01c43b5e22e3b11deb2f0b7615690becc46c62
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 02:00:52 +0530

    :watermelon: cool fruit v2.0.27

--- src/_types.ts
+++ src/_types.ts
@@ -1,6 +1,6 @@
-export type compareFn<V>      = (a: V, b: V) => number;
-export type calledFn<K, V>    = (v: V, k: K, x: Iterable<[K, V]>) => void;
-export type testFn<K, V>      = (v: V, k: K, x: Iterable<[K, V]>) => boolean;
-export type mapFn<K, V, W>    = (v: V, k: K, x: Iterable<[K, V]>) => V|W;
-export type reduceFn<K, V, W> = (acc: W, v: V, k: K, x: Iterable<[K, V]>) => W;
-export type getFn<V>          = () => V;
+export type compareFn<T>      = (a: T, b: T) => number;
+export type calledFn<T, U>    = (v: U, k: T, x: Iterable<[T, U]>) => void;
+export type testFn<T, U>      = (v: U, k: T, x: Iterable<[T, U]>) => boolean;
+export type mapFn<T, U, V>    = (v: U, k: T, x: Iterable<[T, U]>) => U|V;
+export type reduceFn<T, U, V> = (acc: V, v: U, k: T, x: Iterable<[T, U]>) => V;
+export type getFn<T>          = () => T;
