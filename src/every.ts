c402679382c8c0330098a39a4a2a3efaf4a5ac1e
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 03:05:06 +0530

    :watermelon: cool fruit v2.0.60

--- src/every.ts
+++ src/every.ts
@@ -1,14 +1,25 @@
-import type {testFn} from './_types';
+import type {Entries, testFn} from './_types';
+
+function everyIf<T, U>(x: Entries<T, U>): boolean {
+  for(var [, v] of x)
+    if(!v) return false;
+  return true;
+}
+
+function everyTest<T, U>(x: Entries<T, U>, fn: testFn<T, U>, ths: object=null): boolean {
+  for(var [k, v] of x)
+    if(!fn.call(ths, v, k, x)) return false;
+  return true;
+}
 
 /**
  * Checks if all values satisfy a test.
  * @param x a map
- * @param fn test function (v, k, x)
+ * @param fn test function (v, k ,x)
  * @param ths this argument
  */
-function every<K, V>(x: Iterable<[K, V]>, fn: testFn<K, V>, ths: object=null) {
-  for(var [k, v] of x)
-    if(!fn.call(ths, v, k, x)) return false;
-  return true;
+function every<T, U>(x: Entries<T, U>, fn: testFn<T, U>=null, ths: object=null): boolean {
+  if(fn) return everyTest(x, fn, ths);
+  else return everyIf(x);
 }
 export default every;
