c9099762bc1ad3bf693bb571be9dbac272955f2c
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 02:16:37 +0530

    :watermelon: cool fruit v2.0.37

--- src/partitionAs.ts
+++ src/partitionAs.ts
@@ -2,15 +2,15 @@ import id from './_id';
 import type {mapFn} from './_types';
 
 /**
- * Segregates map keeping similar values together.
- * @param x an array
+ * Segregates values by similarity.
+ * @param x a map
  * @param fn map function (v, k, x)
  * @param ths this argument
  * @returns Map {key => values}
  */
-function partitionOn<K, V, W>(x: Iterable<[K, V]>, fn: mapFn<K, V, W>=null, ths: object=null): Map<W, V[]> {
+function partitionOn<T, U, V>(x: Iterable<[T, U]>, fn: mapFn<T, U, V>=null, ths: object=null): Map<V, U[]> {
   var fn = fn||id;
-  var a = new Map<W, V[]>();
+  var a = new Map<V, U[]>();
   for(var [k, v] of x) {
     var v1 = fn.call(ths, v, k, x);
     if(!a.has(v1)) a.set(v1, []);
