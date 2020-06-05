87f029ea8beaa57a634c52833025075055b2cef8
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 02:22:49 +0530

    :watermelon: cool fruit v2.0.41

--- src/keys.ts
+++ src/keys.ts
@@ -1,10 +1,11 @@
+import {Entries} from './_types';
+
 /**
  * Lists all keys.
  * @param x a map
- * @returns ...keys
  */
-function* keys<K, V>(x: Iterable<[K, V]>): IterableIterator<K> {
-  for(var [k, ] of x)
+function* keys<T, U>(x: Entries<T, U>): IterableIterator<T> {
+  for(var [k] of x)
     yield k;
 }
 export default keys;
