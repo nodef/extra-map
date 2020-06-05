d42b445ea595693431e5a59b13783f0ac0440495
Subhajit Sahu <wolfram77@gmail.com>
2020-06-06 02:33:30 +0530

    :watermelon: cool fruit v2.0.48

--- src/getAll.ts
+++ src/getAll.ts
@@ -3,8 +3,8 @@
  * @param x a map
  * @param ks keys
  */
-function getAll<K, V>(x: Map<K, V>, ks: Iterable<K>): Map<K, V> {
-  var a = new Map<K, V>();
+function getAll<T, U>(x: Map<T, U>, ks: Iterable<T>): Map<T, U> {
+  var a = new Map<T, U>();
   for(var k of ks)
     a.set(k, x.get(k));
   return a;
