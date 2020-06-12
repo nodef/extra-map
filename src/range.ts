dfd77b1b42d25f2db91c1396408714c2cd0b08cf
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 21:22:06 +0530

    :watermelon: cool fruit ;range

--- src/range.ts
+++ src/range.ts
@@ -1,16 +1,25 @@
-import iterableRange from '@extra-iterable/range';
+import id from './_id';
 import cmp from './_cmp';
-import type {compareFn} from './_types';
+import type {compareFn, mapFn, Entries} from './_types';
 
 /**
  * Finds smallest and largest entries.
  * @param x a map
- * @param fn compare function (a, b)
- * @returns [min, max]
+ * @param fc compare function (a, b)
+ * @param fm map function (v, k, x)
+ * @returns [smallest, largest]
  */
-function range<K, V>(x: Iterable<[K, V]>, fn: compareFn<[K, V]>=null): [[K, V], [K, V]] {
-  var fn = fn || cmp;
-  // @ts-ignore
-  return iterableRange(x, (a, b) => fn(a[1], b[1]));
+function range<T, U, V=U>(x: Entries<T, U>, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): [[T, U], [T, U]] {
+  var fc = fc||cmp, fm = fm||id;
+  var mk: T, mu: U, mv: U|V;
+  var nk: T, nu: U, nv: U|V;
+  var i = 0;
+  for(var [k, u] of x) {
+    var v = fm(u, k, x);
+    if(i===0 || fc(v, mv)<0) { mk = k; mu = u; mv = v; }
+    if(i===0 || fc(v, nv)>0) { nk = k; nu = u; nv = v; }
+    i++;
+  }
+  return [[mk, mu], [nk, nu]];
 }
 export default range;
