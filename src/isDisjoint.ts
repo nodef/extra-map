9c53134db73a7cdff88fac82e87d81609b77d211
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 22:07:51 +0530

    :watermelon: cool fruit ;isDisjoint

--- src/isDisjoint.ts
+++ src/isDisjoint.ts
@@ -1,15 +1,11 @@
 /**
- * Checks if maps no have key in common.
- * @param xs maps
+ * Checks if maps have no common keys.
+ * @param x a map
+ * @param y another map
  */
-function isDisjoint<K, V>(...xs: Iterable<[K, V]>[]): boolean {
-  var a = new Set<K>();
-  for(var x of xs) {
-    for(var [k, _] of x) {
-      if(a.has(k)) return false;
-      a.add(k);
-    }
-  }
+function isDisjoint<T, U>(x: Map<T, U>, y: Map<T, U>): boolean {
+  for(var k of x.keys())
+    if(y.has(k)) return false;
   return true;
 }
 export default isDisjoint;
