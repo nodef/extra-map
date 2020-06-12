3f26f3748865eaca8e35ba1cba8688be76810a30
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 20:52:29 +0530

    :watermelon: cool fruit +searchValue

--- src/searchValue.ts
+++ src/searchValue.ts
@@ -3,19 +3,18 @@ import cmp from './_cmp';
 import type {compareFn, mapFn, Entries} from './_types';
 
 /**
- * Finds keys with given value.
+ * Finds key with given value.
  * @param x a map
  * @param v search value
  * @param fc compare function (a, b)
  * @param fm map function (v, k, x)
  */
-function searchValueAll<T, U, V=U>(x: Entries<T, U>, v: U, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): T[] {
+function searchValue<T, U, V=U>(x: Entries<T, U>, v: U, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): T {
   var fc = fc||cmp, fm = fm||id;
-  var v1 = fm(v, null, x), a = [];
+  var v1 = fm(v, null, null);
   for(var [k, u] of x) {
     var u1 = fm(u, k, x);
-    if(fc(u1, v1)===0) a.push(k);
+    if(fc(u1, v1)===0) return k;
   }
-  return a;
 }
-export default searchValueAll;
+export default searchValue;
