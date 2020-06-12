02799a2533ebf70c494e9411528129be325e58ec
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 20:49:37 +0530

    :watermelon: cool fruit +searchValueAll

--- src/searchValueAll.ts
+++ src/searchValueAll.ts
@@ -1,22 +1,21 @@
 import id from './_id';
 import cmp from './_cmp';
-import type {Entries, compareFn, mapFn} from './_types';
+import type {compareFn, mapFn} from './_types';
 
 /**
- * Searches a value throughout.
+ * Finds keys with given value.
  * @param x a map
  * @param v search value
  * @param fc compare function (a, b)
  * @param fm map function (v, k, x)
- * @returns keys of value
  */
-function searchAll<T, U, V=U>(x: Entries<T, U>, v: U, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): T[] {
+function searchValueAll<T, U, V=U>(x: Map<T, U>, v: U, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): T[] {
   var fc = fc||cmp, fm = fm||id;
-  var v1 = fm(v, null, null), a: T[] = [];
+  var v1 = fm(v, null, x), a = [];
   for(var [k, u] of x) {
     var u1 = fm(u, k, x);
     if(fc(u1, v1)===0) a.push(k);
   }
   return a;
 }
-export default searchAll;
+export default searchValueAll;
