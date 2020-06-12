42e9fa45608199a5ab9b12d262f4f0a5e6aaf9af
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 22:36:19 +0530

    :watermelon: cool fruit +findAll

---
+++ src/findAll.ts
@@ -0,0 +1,15 @@
+import type {testFn, Entries} from './_types';
+
+/**
+ * Finds values of entries passing a test.
+ * @param x a map
+ * @param fn test function (v, k, x)
+ * @param ths this argument
+ */
+function findAll<T, U>(x: Entries<T, U>, fn: testFn<T, U>, ths: object=null): U[] {
+  var a = [];
+  for(var [k, v] of x)
+    if(fn.call(ths, v, k, x)) a.push(v);
+  return a;
+}
+export default findAll;
