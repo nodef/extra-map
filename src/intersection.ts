20c94020c792caffa43e994846e45d19e6e614f0
Subhajit Sahu <wolfram77@gmail.com>
2020-06-12 22:13:10 +0530

    :watermelon: cool fruit ;intersection

--- src/intersection.ts
+++ src/intersection.ts
@@ -1,11 +1,19 @@
-import intersection$ from './intersection$';
+import id from './_id';
+import type {combineFn} from './_types';
 
 /**
- * Gives entries present in all maps.
- * @param xs maps
+ * Gives entries present in both maps.
+ * @param x a map
+ * @param y another map
+ * @param fn combine function (a, b)
  */
-function intersection<K, V>(...xs: Iterable<[K, V]>[]): Map<K, V> {
-  var x = new Map<K, V>(xs.shift() || []);
-  return intersection$(x, ...xs);
+function intersection<T, U>(x: Map<T, U>, y: Map<T, U>, fn: combineFn<U>=null): Map<T, U> {
+  var fn = fn||id;
+  var a = new Map();
+  for(var [k, u] of x) {
+    if(!y.has(k)) continue;
+    a.set(k, fn(u, y.get(k)));
+  }
+  return a;
 }
 export default intersection;
