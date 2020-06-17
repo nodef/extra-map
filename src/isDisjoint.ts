33fe0819a7d1f0d9126b9cc89a277d28df1694d6
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 10:18:04 +0530

    :watermelon: cool fruit ;isDisjoint

--- src/isDisjoint.ts
+++ src/isDisjoint.ts
@@ -1,11 +1,13 @@
+import {Entries} from './_types';
+
 /**
  * Checks if maps have no common keys.
  * @param x a map
  * @param y another map
  */
-function isDisjoint<T, U>(x: Map<T, U>, y: Map<T, U>): boolean {
-  for(var k of x.keys())
-    if(y.has(k)) return false;
+function isDisjoint<T, U>(x: Map<T, U>, y: Entries<T, U>): boolean {
+  for(var [k] of y)
+    if(x.has(k)) return false;
   return true;
 }
 export default isDisjoint;
