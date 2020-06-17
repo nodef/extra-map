ba311e295013c031435672dfac5f55bdc899796d
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 10:44:47 +0530

    :watermelon: cool fruit ;difference

--- src/difference$.ts
+++ src/difference$.ts
@@ -1,11 +1,13 @@
+import type {Entries} from './_types';
+
 /**
  * Gives entries of map not present in another.
  * @param x a map (updated)
  * @param y another map
  * @returns x
  */
-function difference$<T, U>(x: Map<T, U>, y: Map<T, U>): Map<T, U> {
-  for(var k of y.keys())
+function difference$<T, U>(x: Map<T, U>, y: Entries<T, U>): Map<T, U> {
+  for(var [k] of y)
     x.delete(k);
   return x;
 }
--- src/difference.ts
+++ src/difference.ts
@@ -1,12 +1,12 @@
+import difference$ from './difference$';
+import type {Entries} from './_types';
+
 /**
  * Gives entries of map not present in another.
  * @param x a map
  * @param y another map
  */
-function difference<T, U>(x: Map<T, U>, y: Map<T, U>): Map<T, U> {
-  var a = new Map();
-  for(var [k, v] of x)
-    if(!y.has(k)) a.set(k, v);
-  return a;
+function difference<T, U>(x: Entries<T, U>, y: Entries<T, U>): Map<T, U> {
+  return difference$(new Map(x), y);
 }
 export default difference;
