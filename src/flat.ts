0a4e7edd04ce491094bdfc492af212bf8b039ac2
Subhajit Sahu <wolfram77@gmail.com>
2020-06-18 12:05:01 +0530

    :watermelon: cool fruit pre example

--- src/flat.ts
+++ src/flat.ts
@@ -1,10 +1,12 @@
+import id from './_id';
 import is from './is';
-import type {testFn, Entries} from './_types';
+import type {mapFn, testFn, Entries} from './_types';
 
-function flatTo<T>(x: Entries<T, any>, n: number, ft: testFn<T, any>, a: Map<T, any>): Map<T, any> {
+function flatTo<T>(x: Entries<T, any>, n: number, fm: mapFn<T, any, any>, ft: testFn<T, any>, a: Map<T, any>): Map<T, any> {
   for(var [k, v] of x) {
-    if(n!==0 && ft(v, k, x)) flatTo(v, n-1, ft, a);
-    else a.set(k, v);
+    var v1 = fm(v, k, x);
+    if(n!==0 && ft(v1, k, x)) flatTo(v1, n-1, fm, ft, a);
+    else a.set(k, v1);
   }
   return a;
 }
@@ -13,9 +15,11 @@ function flatTo<T>(x: Entries<T, any>, n: number, ft: testFn<T, any>, a: Map<T, 
  * Flattens nested map to given depth.
  * @param x a nested map
  * @param n maximum depth (-1 => all)
+ * @param fm map function (v, k, x)
  * @param ft test function (v, k, x)
  */
-function flat<T>(x: Entries<T, any>, n: number=-1, ft: testFn<T, any>=null): Map<T, any> {
-  return flatTo(x, n, ft||is, new Map());
+function flat<T>(x: Entries<T, any>, n: number=-1, fm: mapFn<T, any, any>=null, ft: testFn<T, any>=null): Map<T, any> {
+  var fm = fm||id, ft = ft||is;
+  return flatTo(x, n, fm, ft, new Map());
 }
 export default flat;
