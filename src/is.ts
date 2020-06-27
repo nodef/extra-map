ecbd13e0e09f039c46da9dbc6594794c061b2e33
Subhajit Sahu <wolfram77@gmail.com>
2020-06-28 01:09:12 +0530

    :coconut: chutney +getPath, setPath$, removePath$

--- src/is.ts
+++ src/is.ts
@@ -2,7 +2,7 @@
  * Checks if value is map.
  * @param v value
  */
-function is(v: any): boolean {
+function is(v: any): v is Map<any, any> {
   return v instanceof Map;
 }
 export default is;
---
+++ src/setPath$.ts
@@ -0,0 +1,17 @@
+import is from './is';
+import getPath from './getPath';
+import {last} from 'extra-array';
+
+/**
+ * Sets value at path in a nested map.
+ * @param x a map (updated)
+ * @param p path
+ * @param v value
+ * @returns x
+ */
+function setPath$<T>(x: Map<T, any>, p: T[], v: any): Map<T, any> {
+  var y = getPath(x, p.slice(0, -1));
+  if(is(y)) y.set(last(p), v);
+  return x;
+}
+export default setPath$;
