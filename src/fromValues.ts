2ff6bacc490aa32b84fb50c64d6536ea567a2a36
Subhajit Sahu <wolfram77@gmail.com>
2020-06-27 15:39:44 +0530

    :coconut: chutney ;fromValues

--- src/fromValues.ts
+++ src/fromValues.ts
@@ -1,16 +1,20 @@
 import id from './_id';
+import {from$} from 'extra-array';
 import type {arrayMapFn} from './_types';
+
+function fromValuesArray<T, U=T>(vs: T[], fm: arrayMapFn<T, T|U>) {
+  var a = new Map(), ks = vs.map(fm);
+  for(var i=0, I=vs.length; i<I; i++)
+    a.set(ks[i], vs[i]);
+  return a;
+}
 
 /**
  * Creates a map from values.
  * @param vs values
  * @param fm map function (v, i, x)
  */
-function fromValues<T, U=T>(vs: T[], fm: arrayMapFn<T, T|U>=null) {
-  var fm = fm||id;
-  var a = new Map(), ks = vs.map(fm);
-  for(var i=0, I=vs.length; i<I; i++)
-    a.set(ks[i], vs[i]);
-  return a;
+function fromValues<T, U=T>(vs: Iterable<T>, fm: arrayMapFn<T, T|U>=null) {
+  return fromValuesArray(from$(vs), fm||id);
 }
 export default fromValues;
